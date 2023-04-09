0e2089fce363d3007232ab706b4fe018ebe06579
Karthi <karthiknselva07@gmail.com>
2023-04-10 01:33:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ShoppingList from './components/ShoppingList';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+      <ShoppingList />
+      </header>
+      
+        
+    
+    </div>
+  );
+}
+
+export default App;
